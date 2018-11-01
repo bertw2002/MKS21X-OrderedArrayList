669d460930d917515e61c36409d3436f88cbf897
Sublime <other@Alberts-MacBook-Pro.local>
2018-11-01 11:01:25 -0400

    made some code

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,1 +1,31 @@
+import java.util.ArrayList;
+public class NoNullArrayList<T> extends ArrayList<T>{
+  public NullArrayList(){
+    super();
+  }
+  public NullArrayList(int startingCapacity){
+    super(startingCapacity);
+  }
+  public void set(int index, T element){
+    if (element == null){
+      throw new IllegalArgumentException();
+    }else{
+    super.set(index, element);
+    }
+  }
+  public boolean add(T element){
+    if (element == null){
+      throw new IllegalArgumentException();
+    }else{
+      super.add(element);
+    }
+  }
+  public void add(int index, T element){
+    if (element == null){
+      throw new IllegalArgumentException();
+    }else{
+      super.add(index, element);
+    }
+  }
 
+}

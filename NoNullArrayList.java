39080f3d4587de1991a38939e151b2a969936238
Sublime <other@Alberts-MacBook-Pro.local>
2018-11-02 11:00:49 -0400

    more code

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -6,18 +6,18 @@ public class NoNullArrayList<T> extends ArrayList<T>{
   public NoNullArrayList(int startingCapacity){
     super(startingCapacity);
   }
-  public void set(int index, T element){
+  public T set(int index, T element){
     if (element == null){
       throw new IllegalArgumentException();
     }else{
-    super.set(index, element);
+      return super.set(index, element);
     }
   }
   public boolean add(T element){
     if (element == null){
       throw new IllegalArgumentException();
     }else{
-      super.add(element);
+      return super.add(element);
     }
   }
   public void add(int index, T element){
--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,1 +1,14 @@
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
+  public OrderedArrayList(){
+    super();
+  }
+  public OrderedArrayList(int startingCapacity){
+    super(startingCapacity);
+  }
+  public boolean add(T element){
 
+  }
+  public void add(int index, T element){
+
+  }
+}

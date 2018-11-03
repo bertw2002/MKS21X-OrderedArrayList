2bf2a29bf02ef51f7b33d91df743ede413e8ad85
Sublime <other@Alberts-MacBook-Pro.local>
2018-11-03 17:22:48 -0400

    more code

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -5,10 +5,37 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int startingCapacity){
     super(startingCapacity);
   }
-  public boolean add(T element){
-
+  public int whatindex(T element){
+    if (element.compareTo(super.get(super.size() - 1)) == 1){
+      return super.size();//last element
+    }
+    for (int x = 0;x < super.size() - 1; x++){
+      if (element.compareTo(super.get(x)) == 1 && element.compareTo(super.get(x + 1)) == -1){
+        return x + 1;
+      }
+    }
+    return 0;
   }
-  public void add(int index, T element){
+  public boolean add(T element){
+    if (whatindex(element) == super.size()){
+      super.add(element);
+      return true;
+    }
+    super.add(whatindex(element), element);
+    return true;
+  }
 
+  public void add(int index, T element){
+    if (whatindex(element) == super.size()){
+      super.add(element);
+    }
+    super.add(whatindex(element), element);
+  }
+
+  public T sorted(){
+    OrderedArrayList<T> newarr = new OrderedArrayList<T>(super.size());
+  }
+  public T set(int index, T element){
+    super(set(index, element));
   }
 }

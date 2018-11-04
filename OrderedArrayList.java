f69092a941e634d1bfbfc389f405010c7ee4f591
Sublime <other@Alberts-MBP.fios-router.home>
2018-11-04 00:07:57 -0400

    erros....

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -6,6 +6,9 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     super(startingCapacity);
   }
   public int whatindex(T element){
+    if (element == null){
+      throw new IllegalArgumentException();
+    }
     if (element.compareTo(super.get(super.size() - 1)) == 1){
       return super.size();//last element
     }
@@ -34,11 +37,13 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
 
   public void sorted(int index, T element){
     super.remove(index);
-    if (element.compareTo(super.get(0)) == -1){
-      super.add(0, element);
-    }
-    if (element.compareTo(super.get(super.size() - 1)) == 1){
+    if (!(element.compareTo(super.get(0)) == -1)){
+      if (super.size() != 0){
+        super.add(0, element);
+      }else{super.add(element);}
+    }else if(element.compareTo(super.get(super.size() - 1)) == 1){
       super.add(element);
+
     }
     for (int x = 0; x < super.size() - 1; x++){
       if (element.compareTo(super.get(x)) >= 0 && element.compareTo(super.get(x + 1)) <= 0){
@@ -49,12 +54,11 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
 
   }
   public T set(int index, T element){
-    OrderedArrayList<T> newarr = new OrderedArrayList<T>(super.size());
-    for (int x = 0; x < super.size();x++){
-      newarr.set(x, super.get(x));
+    if (index >= super.size()){
+      throw new IndexOutOfBoundsException();
     }
-    super.set(index, element);
+    T var = super.set(index, element);
     sorted(index, element);
-    return newarr.set(index,element);
+    return var;
   }
 }
